68cd4265b0d09cede02513583ebda6d9e1c89776
hellcy <chengyuan82281681@hotmail.com>
2021-11-04 15:43:01 +1100

    refactoring Services extends CrudService

--- pet-clinic-data/src/main/java/com/yuancheng/petclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/com/yuancheng/petclinic/services/OwnerService.java
@@ -2,14 +2,6 @@ package com.yuancheng.petclinic.services;
 
 import com.yuancheng.petclinic.models.Owner;
 
-import java.util.Set;
-
-public interface OwnerService {
+public interface OwnerService extends CrudService<Owner, Long>{
   Owner findByLastName(String lastName);
-
-  Owner findById(Long id);
-
-  Owner save(Owner owner);
-
-  Set<Owner> findAll();
 }
