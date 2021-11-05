f41809da38bee5b86714059fc8e266327911623b
hellcy <chengyuan82281681@hotmail.com>
2021-11-05 17:29:55 +1100

    adding Vet list and updated controller

--- pet-clinic-web/src/main/java/com/yuancheng/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/yuancheng/petclinic/controllers/VetController.java
@@ -1,13 +1,23 @@
 package com.yuancheng.petclinic.controllers;
 
+import com.yuancheng.petclinic.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class VetController {
 
+  private final VetService vetService;
+
+  public VetController(VetService vetService) {
+    this.vetService = vetService;
+  }
+
   @RequestMapping({"/vets/index", "/vets/index.html", "/vets"})
-  public String listVets() {
+  public String listVets(Model model) {
+    model.addAttribute("vets", vetService.findAll());
+
     return "vets/index";
   }
 }
