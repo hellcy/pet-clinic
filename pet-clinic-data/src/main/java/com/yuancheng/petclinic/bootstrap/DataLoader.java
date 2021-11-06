24c84077b9f14b30e007849193fc0dc78d251c60
hellcy <chengyuan82281681@hotmail.com>
2021-11-07 10:58:30 +1100

    Refactored Map impl to abstract ID generation

--- pet-clinic-data/src/main/java/com/yuancheng/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-data/src/main/java/com/yuancheng/petclinic/bootstrap/DataLoader.java
@@ -20,14 +20,12 @@ public class DataLoader implements CommandLineRunner {
   @Override
   public void run(String... args) throws Exception {
     Owner owner1 = new Owner();
-    owner1.setId(1L);
     owner1.setFirstName("Yuan");
     owner1.setLastName("Cheng");
 
     ownerService.save(owner1);
 
     Owner owner2 = new Owner();
-    owner2.setId(2L);
     owner2.setFirstName("Nan");
     owner2.setLastName("Yang");
 
@@ -36,14 +34,12 @@ public class DataLoader implements CommandLineRunner {
     System.out.println("Loaded owners...");
 
     Vet vet1 = new Vet();
-    vet1.setId(1L);
     vet1.setFirstName("Luke");
     vet1.setLastName("Zhang");
 
     vetService.save(vet1);
 
     Vet vet2 = new Vet();
-    vet2.setId(2L);
     vet2.setFirstName("Meixi");
     vet2.setLastName("Zhu");
 
