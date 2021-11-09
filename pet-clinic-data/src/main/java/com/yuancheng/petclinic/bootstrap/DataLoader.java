79d190dac9b3c5a405994d19ce3c53ec63eb6267
hellcy <chengyuan82281681@hotmail.com>
2021-11-09 22:06:58 +1100

    added PetTypes to DataLoader.java

--- pet-clinic-data/src/main/java/com/yuancheng/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-data/src/main/java/com/yuancheng/petclinic/bootstrap/DataLoader.java
@@ -1,8 +1,10 @@
 package com.yuancheng.petclinic.bootstrap;
 
 import com.yuancheng.petclinic.models.Owner;
+import com.yuancheng.petclinic.models.PetType;
 import com.yuancheng.petclinic.models.Vet;
 import com.yuancheng.petclinic.services.OwnerService;
+import com.yuancheng.petclinic.services.PetTypeService;
 import com.yuancheng.petclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -11,10 +13,12 @@ import org.springframework.stereotype.Component;
 public class DataLoader implements CommandLineRunner {
   private final OwnerService ownerService;
   private final VetService vetService;
+  private final PetTypeService petTypeService;
 
-  public DataLoader(OwnerService ownerService, VetService vetService) {
+  public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
     this.ownerService = ownerService;
     this.vetService = vetService;
+    this.petTypeService = petTypeService;
   }
 
   @Override
@@ -46,5 +50,13 @@ public class DataLoader implements CommandLineRunner {
     vetService.save(vet2);
 
     System.out.println("Loaded vets...");
+
+    PetType dog = new PetType();
+    dog.setName("Dog");
+    PetType savedDogPetType = petTypeService.save(dog);
+
+    PetType cat = new PetType();
+    dog.setName("Cat");
+    PetType savedCatPetType = petTypeService.save(cat);
   }
 }
