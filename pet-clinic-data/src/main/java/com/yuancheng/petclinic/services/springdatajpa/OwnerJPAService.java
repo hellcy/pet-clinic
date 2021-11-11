089a60555073594725c77819b3a4ab15f8b8af9d
hellcy <chengyuan82281681@hotmail.com>
2021-11-11 13:16:53 +1100

    added spring data JPA service for Owners

---
+++ pet-clinic-data/src/main/java/com/yuancheng/petclinic/services/springdatajpa/OwnerJPAService.java
@@ -0,0 +1,59 @@
+package com.yuancheng.petclinic.services.springdatajpa;
+
+import com.yuancheng.petclinic.models.Owner;
+import com.yuancheng.petclinic.repositories.OwnerRepository;
+import com.yuancheng.petclinic.repositories.PetRepository;
+import com.yuancheng.petclinic.repositories.PetTypeRepository;
+import com.yuancheng.petclinic.services.OwnerService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class OwnerJPAService implements OwnerService {
+  private final OwnerRepository ownerRepository;
+  private final PetRepository petRepository;
+  private final PetTypeRepository petTypeRepository;
+
+  public OwnerJPAService(OwnerRepository ownerRepository, PetRepository petRepository, PetTypeRepository petTypeRepository) {
+    this.ownerRepository = ownerRepository;
+    this.petRepository = petRepository;
+    this.petTypeRepository = petTypeRepository;
+  }
+
+  @Override
+  public Set<Owner> findAll() {
+    Set<Owner> owners = new HashSet<>();
+    ownerRepository.findAll().iterator().forEachRemaining(owners::add);
+    return owners;
+  }
+
+  @Override
+  public Owner findById(Long aLong) {
+    return ownerRepository.findById(aLong).orElse(null);
+  }
+
+  @Override
+  public Owner save(Owner object) {
+    return ownerRepository.save(object);
+  }
+
+  @Override
+  public void delete(Owner object) {
+    ownerRepository.delete(object);
+  }
+
+  @Override
+  public void deleteById(Long aLong) {
+    ownerRepository.deleteById(aLong);
+  }
+
+  @Override
+  public Owner findByLastName(String lastName) {
+    return ownerRepository.findByLastName(lastName);
+  }
+}
