e831c6a205b166aa0b6fa8e4b8d46a1e05163f25
hellcy <chengyuan82281681@hotmail.com>
2021-11-11 11:42:59 +1100

    update base entity with JPA annotations

--- pet-clinic-data/src/main/java/com/yuancheng/petclinic/models/BaseEntity.java
+++ pet-clinic-data/src/main/java/com/yuancheng/petclinic/models/BaseEntity.java
@@ -1,8 +1,19 @@
 package com.yuancheng.petclinic.models;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
+/*
+  Telling JPA that this is a base class that other classes will inherit
+  Do not add a table for it
+ */
+@MappedSuperclass
 public class BaseEntity implements Serializable {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
   public Long getId() {
